ee11de39ee190b2040df16abbf0d7fa21b1b579e
kaurkiran123 <146619004+kaurkiran123@users.noreply.github.com>
2024-02-27 11:42:34 +0530

    Add files via upload

---
+++ kids.js
@@ -0,0 +1,325 @@
+
+
+      
+function likeOn1()
+{
+    const element = document.getElementById("on1");
+  element.src= "bookmark (1).png";
+}
+
+        
+function cardCangeimg1()
+{
+    const element = document.getElementById("js-img-1");
+  element.src= "c2.jpg";
+}
+         
+function cardCangeimg2()
+{
+    const element = document.getElementById("js-img-1");
+  element.src= "c3.jpg";
+}
+         
+function cardCangeimg3()
+{
+    const element = document.getElementById("js-img-1");
+  element.src= "c1.jpg";
+}
+  
+     
+function likeOn2()
+{
+    const element = document.getElementById("on2");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg4()
+{
+    const element = document.getElementById("js-img-2");
+  element.src= "c6.jfif";
+}
+         
+function cardCangeimg5()
+{
+    const element = document.getElementById("js-img-2");
+  element.src= "c5.jfif";
+}
+         
+function cardCangeimg6()
+{
+    const element = document.getElementById("js-img-2");
+  element.src= "c4.avif";
+}
+   
+   
+     
+function likeOn3()
+{
+    const element = document.getElementById("on3");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg7()
+{
+    const element = document.getElementById("js-img-3");
+  element.src= "c8.webp";
+}
+         
+function cardCangeimg8()
+{
+    const element = document.getElementById("js-img-3");
+  element.src= "c7.webp";
+}
+         
+function cardCangeimg9()
+{
+    const element = document.getElementById("js-img-3");
+  element.src= "c9.webp";
+}
+   
+    
+function likeOn4()
+{
+    const element = document.getElementById("on4");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg10()
+{
+    const element = document.getElementById("js-img-4");
+  element.src= "c12.jfif";
+}
+         
+function cardCangeimg11()
+{
+    const element = document.getElementById("js-img-4");
+  element.src= "c10.webp";
+}
+         
+function cardCangeimg12()
+{
+    const element = document.getElementById("js-img-4");
+  element.src= "c11.jfif";
+}
+   
+   
+   
+function likeOn5()
+{
+    const element = document.getElementById("on5");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg13()
+{
+    const element = document.getElementById("js-img-5");
+  element.src= "c15.jfif";
+}
+         
+function cardCangeimg14()
+{
+    const element = document.getElementById("js-img-5");
+  element.src= "c13.jfif";
+}
+         
+function cardCangeimg15()
+{
+    const element = document.getElementById("js-img-5");
+  element.src= "c14.jfif";
+}
+   
+   
+function likeOn6()
+{
+    const element = document.getElementById("on5");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg16()
+{
+    const element = document.getElementById("js-img-6");
+  element.src= "k13.jpeg";
+}
+         
+function cardCangeimg17()
+{
+    const element = document.getElementById("js-img-6");
+  element.src= "k15.jpeg";
+}
+         
+function cardCangeimg18()
+{
+    const element = document.getElementById("js-img-6");
+  element.src= "k16.jpeg";
+}
+
+
+ 
+function likeOn7()
+{
+    const element = document.getElementById("on7");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg19()
+{
+    const element = document.getElementById("js-img-7");
+  element.src= "c19.jfif";
+}
+         
+function cardCangeimg20()
+{
+    const element = document.getElementById("js-img-7");
+  element.src= "c17.jfif";
+}
+         
+function cardCangeimg21()
+{
+    const element = document.getElementById("js-img-7");
+  element.src= "c18.jfif";
+}
+   
+  
+function likeOn8()
+{
+    const element = document.getElementById("on8");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg22()
+{
+    const element = document.getElementById("js-img-8");
+  element.src= "c20.jpg";
+}
+         
+function cardCangeimg23()
+{
+    const element = document.getElementById("js-img-8");
+  element.src= "c21.jfif";
+}
+         
+function cardCangeimg24()
+{
+    const element = document.getElementById("js-img-8");
+  element.src= "c22.jfif";
+}
+   
+   
+function likeOn9()
+{
+    const element = document.getElementById("on9");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg25()
+{
+    const element = document.getElementById("js-img-9");
+  element.src= "c24.jfif";
+}
+         
+function cardCangeimg26()
+{
+    const element = document.getElementById("js-img-9");
+  element.src= "c23.webp";
+}
+         
+function cardCangeimg27()
+{
+    const element = document.getElementById("js-img-9");
+  element.src= "c25.jfif";
+}
+    
+function likeOn10()
+{
+    const element = document.getElementById("on10");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg28()
+{
+    const element = document.getElementById("js-img-10");
+  element.src= "k21.jpeg";
+}
+         
+function cardCangeimg29()
+{
+    const element = document.getElementById("js-img-10");
+  element.src= "k22.jpeg";
+}
+         
+function cardCangeimg30()
+{
+    const element = document.getElementById("js-img-10");
+  element.src= "c26.jfif";
+}
+   
+ 
+function likeOn11()
+{
+    const element = document.getElementById("on11");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg31()
+{
+    const element = document.getElementById("js-img-11");
+  element.src= "k21.jpeg";
+}
+         
+function cardCangeimg32()
+{
+    const element = document.getElementById("js-img-11");
+  element.src= "k23.jpeg";
+}
+         
+function cardCangeimg33()
+{k24.jpe
+    const element = document.getElementById("js-img-11");
+  element.src= "g";
+}
+   
+ 
+function likeOn12()
+{
+    const element = document.getElementById("on12");
+  element.src= "bookmark (1).png";
+}
+
+       
+function cardCangeimg34()
+{
+    const element = document.getElementById("js-img-12");
+  element.src= "c31.jfif";
+}
+         
+function cardCangeimg35()
+{
+    const element = document.getElementById("js-img-12");
+  element.src= "k19.jpeg";
+}
+         
+function cardCangeimg36()
+{
+    const element = document.getElementById("js-img-12");
+  element.src= "c32.jfif";
+}
+   
+
+
+
+
+
+
+   
+   
+  
